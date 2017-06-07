a1b49e24e838ff3fdc8d2ddeb6c229ac767d262b
Unknown <nelsontherichboy@gmail.com>
2017-06-06 20:19:19 -0600

    ejercicio 2 y 3

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/0801199214375', function(req,res,next){
+  var examenNelson = [
+    {
+      "Nombre completo":"Nelson Adalberto Amador Sorto",
+      "Numero de cuenta": "0801199214375",
+      "Correo": "nelson15amador15@gmail.com",
+    }
+    ];
+  res.json(examenNelson);
+});
+
+router.get('/prueba', function(req,res,next){
+  res.render('suma',{"numero1":"","numero2":"","msg":""});
+});
+
+var sumas = [];
+
+router.post('/suma',function(req,res,next){
+  console.log(req.body);
+    sumas.push(req.body.numero1);
+    sumas.push(req.body.numero2);
+    var msgs = parseInt(sumas[0]+parseInt(sumas[1]));
+    var rtObject = {};
+    rtObject.numero1 = req.body.numero1;
+    rtObject.numero2 = req.body.numero2;
+    rtObject.msg= msgs;
+  res.render('suma', rtObject);
+});
+
 module.exports = router;
